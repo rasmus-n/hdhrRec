0644980df7eda4dd03272b3cd148fe15346fa057
Rasmus Nielsen <rasmus_n@pc.dk>
2012-11-17 16:30:07 +0100

    Completed some more features of the web interface

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This is primarely a personal project to satisfy my own TV recording needs.
 
 My recording hardware consist of an [alix3d2](http://www.pcengines.ch/alix3d2.htm) from PC Engines, a [HDHomeRun Dual](http://www.silicondust.com/products/models/hdhr3-eu/) from SiliconDust and a USB harddrive.
 
-The alix3d2 is a single board computer with a 500MHz CPU and 256MB of RAM, it has no video output.
+The alix3d2 is a single board computer with a 500MHz CPU and 256MB of RAM, it has no video output. It should be possible to make hdhrRec run on most devices that can run python: In my setup, recording a TV program only puts about 6% load on the CPU.
 
 The purpose of this project is to provide a relatively simple, lightweight, automatic recording scheduler for HDHomeRun devices, suitable for running headless on low power hardware.
 
@@ -23,14 +23,14 @@ I stopped using MythTV because:
 * Not well suited for headless operation.
 * Not "friendly" to general purpose tools.
 
-Compared to MythTV, I really like TVHeadend, but stopped using in because:
+Compared to MythTV, I really like TVHeadend, but stopped using it because:
 * No native support for HDHomeRun devices (it did work with [dvbhdhomerun](http://sourceforge.net/apps/trac/dvbhdhomerun/)).
 * Would rewrap the streams in MKV.
 * Would fail to identify the audio on some TV channels, and thus not put the audio stream in the MKV.
 
 ## Features
 
-* Calls an external script to update the program table (a script for dr.dk (danish national tv) is provided).
+* Calls an external script to update the program table (a script for dr.dk, danish national tv, is provided).
 * Decides what to record based on rules that may add "weight" to the programs in the program table.
 * Can record multiple programs simultaneously on the same MUX (MultiRec).
 * Should handle multiple HDHomeRun devices (not tested).
@@ -53,7 +53,7 @@ The scheduler and the web interface are two independent applications who "commun
 
 The scheduler is written in python and only depends on one module not part of the python standard modules; to get satisfactory performance the actual "recording", i.e. filtering of streams and writing to file, is done in a python module written i C.
 
-Building the python module requires development files for python and libhdhomerun.
+Building the python module requires development files for python and libhdhomerun. Due to some problems with dropped UDP packages, the recorder module will try to increase the RX buffer size, so it may be necessary . 
 
 The web interface is also written in python, using the [web.py](http://webpy.org/) framework.
 
@@ -67,4 +67,5 @@ As stated above, this project was started because I was not satisfied with the o
 * Robustness against bad input from web interface.
 * No prioritising in case the number of required tuners exceeds the number of available tuner (not tested, it might even cause the application to crash).
 * Script to insert tv program data from a XMLTV-file.
+* Mobile phone friendly web interface
 * ...
--- www/hdhrWeb.py
+++ www/hdhrWeb.py
@@ -7,7 +7,6 @@ code = locale.getpreferredencoding()
 import web
 from web import form
 from datetime import datetime
-import urllib
 
 render = web.template.render('templates/', base='layout')
 db = web.database(dbn='sqlite', db='../tv.sqlite')
@@ -28,15 +27,31 @@ profile_form = form.Form (
   form.Textbox('post_record', description='Post rec.'),
 )
 
+stream_form = form.Form (
+  form.Textbox('name'     , description='Name'),
+  form.Textbox('mux'      , description='Channel'),
+  form.Textbox('pid'      , description='PID'),  
+  form.Textbox('video'    , description='VID'),
+  form.Textbox('audio'    , description='AID'),
+  form.Textbox('subtitles', description='SID'),
+)
+
 urls = (
-  '/'                     , 'start',
-  '/streams'              , 'streams',
-  '/rules'                , 'rules',
-  '/rules/add'            , 'rule_add',
-  '/rules/edit/([0-9]+)'  , 'rule_edit',
-  '/rec'                  , 'rec',
-  '/profiles'             , 'profiles',
-  '/profiles/add'         , 'profile_add',
+  '/'                       , 'start',
+  '/streams'                , 'streams',
+  '/streams/add'            , 'stream_add',
+  '/streams/edit/([0-9]+)'  , 'stream_edit',
+  '/streams/delete/([0-9]+)', 'stream_delete',
+  '/rules'                  , 'rules',
+  '/rules/add'              , 'rule_add',
+  '/rules/edit/([0-9]+)'    , 'rule_edit',
+  '/rules/delete/([0-9]+)'  , 'rule_delete',
+  '/rec'                    , 'rec',
+  '/rec/edit/([0-9]+)'      , 'rec_edit',
+  '/profiles'               , 'profiles',
+  '/profiles/add'           , 'profile_add',
+  '/profiles/edit/([0-9]+)' , 'profile_edit',
+  '/profiles/delete/([0-9]+)' , 'profile_delete',
 )
 
 class start:
@@ -53,7 +68,47 @@ class streams:
   def GET(self):
     streams = db.select('channels', what='rowid,*')
     return render.streams(streams)
+
+class stream_add:
+  def GET(self):
+    f = stream_form()
+    return render.stream_add(f)
     
+  def POST(self):
+    f = stream_form()
+    if f.validates():
+      db.insert('channels', **f.d)
+      raise web.seeother('/streams')
+    else:
+      return render.stream_add(f)
+
+class stream_edit:
+  def GET(self, stream_nr):
+    f = stream_form()
+    d = dict(id = stream_nr);
+    stream = db.select('channels', d, where='rowid = $id', what='rowid,*')
+    f.fill(stream[0])
+    return render.stream_edit(f, stream_nr)
+    
+  def POST(self, stream_nr):
+    f = stream_form()
+    if f.validates():
+      d = dict(id = stream_nr)
+      db.update('channels', 'rowid=$id', d, **f.d)
+      raise web.seeother('/streams')
+    else:
+      return render.stream_edit(f)
+
+class stream_delete:
+  def POST(self, stream_nr):
+    f = stream_form()
+    if f.validates():
+      d = dict(id = stream_nr)
+      db.delete('channels', 'rowid=$id', vars=d)
+      raise web.seeother('/streams')
+    else:
+      return render.stream_edit(f)
+
 class rules:
   def GET(self):
     rules = db.select('rules', what='rowid,*')
@@ -79,13 +134,24 @@ class rule_edit:
     d = dict(id = rule_nr);
     rule = db.select('rules', d, where='rowid = $id', what='rowid,*')
     f.fill(rule[0])
-    return render.rule_edit(f)
+    return render.rule_edit(f, rule_nr)
     
   def POST(self, rule_nr):
     f = rule_form()
     if f.validates():
       d = dict(id = rule_nr)
       db.update('rules', 'rowid=$id', d, **f.d)
+      db.update('table_update_times', where='rowid=1', rules=datetime.now())
+      raise web.seeother('/rules')
+    else:
+      return render.rule_edit(f)
+
+class rule_delete:
+  def POST(self, rule_nr):
+    f = rule_form()
+    if f.validates():
+      d = dict(id = rule_nr)
+      db.delete('rules', 'rowid=$id', vars=d)
       db.update('table_update_times', where='rowid=1', rules=datetime.now())
       raise web.seeother('/rules')
     else:
@@ -109,11 +175,42 @@ class profile_add:
     else:
       return render.profile_add(f)
 
+class profile_edit:
+  def GET(self, profile_nr):
+    f = profile_form()
+    d = dict(id = profile_nr);
+    profile = db.select('profiles', d, where='rowid = $id', what='rowid,*')
+    f.fill(profile[0])
+    return render.profile_edit(f, profile_nr)
+    
+  def POST(self, profile_nr):
+    f = profile_form()
+    if f.validates():
+      d = dict(id = profile_nr)
+      db.update('profiles', 'rowid=$id', d, **f.d)
+      raise web.seeother('/profiles')
+    else:
+      return render.profile_edit(f)
+
+class profile_delete:
+  def POST(self, profile_nr):
+    f = profile_form()
+    if f.validates():
+      d = dict(id = profile_nr)
+      db.delete('profiles', 'rowid=$id', vars=d)
+      raise web.seeother('/profiles')
+    else:
+      return render.profile_edit(f)
+
 class rec:
   def GET(self):
     rec = db.select('recordings', what='rowid,*', order='program_start')
     return render.rec(rec)
 
+class rec_edit:
+  def GET(self, rec_nr):
+    raise web.seeother('/rec')
+    
 app = web.application(urls, globals())
 
 if __name__ == "__main__":
