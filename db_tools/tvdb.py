cb30c828b6ecb047cc22dc359e89ae7444e05887
Rasmus Nielsen <rasmus_n@pc.dk>
2012-11-24 08:34:12 +0100

    A little more robust and intelligent update of program list

--- db_tools/tvdb.py
+++ db_tools/tvdb.py
@@ -16,6 +16,7 @@ channelMap = {
 'DRK'       : ('TVK',"w_"),
 'Ramasjang' : ('TVR',"w_"),
 'DRHD'      : ('TVH',"w_"),
+'TV2OJ'     : ('r371',"r_"),
 }
 
 dateFormatDR = "%Y-%m-%dT%H:%M:%S"
@@ -23,10 +24,13 @@ dateFormatDR = "%Y-%m-%dT%H:%M:%S"
 #db.execute("CREATE TABLE plan (ch text, st datetime, et datetime, title text, subtitle text, description text)")
 
 #channels = channelMap.keys()
-channels = ('DR1','DR2','DRK','Ramasjang','DRHD')
-numberOfDays = 1
+channels = ('DR1','DR2','DRK','Ramasjang','DRHD','TV2OJ')
+numberOfDays = 2
 
 now = datetime.now()
+
+db.execute('DELETE FROM programs WHERE (end < ?)', (now,))
+db.commit()
 
 for channel in channels:
   if not channel in channelMap:
@@ -38,7 +42,10 @@ for channel in channels:
     date=(datetime.now()+timedelta(days=day)).strftime("%Y-%m-%d")
 #    request = "http://www.dr.dk/tjenester/programoversigt/DBService.ashx/getSchedule?channel_source_url=%s&broadcastDate=%s" % (source, date)
     request = "http://www.dr.dk/tv/oversigt/json/guide/schedule?startTimesectionId=0&days=%d&channelid=%s" % (day,source)
-    data = json.loads(urllib.urlopen(request).read())
+    try:
+      data = json.loads(urllib.urlopen(request).read())
+    except:
+      continue
 
 #    if 'result' in data and data['result'] != []:
 #      programs = data['result']
@@ -49,6 +56,8 @@ for channel in channels:
     
     TimeSections = data['TimeSection']
     dbdata = []
+    dbdata_start = []
+    dbdata_end = []
     
     for TimeSection in TimeSections:
       programs = TimeSection['Programs']
@@ -71,9 +80,17 @@ for channel in channels:
         d.append(program['Punchline'])
         if d[3] > now: # 3 = end
           dbdata.append(d)
+          dbdata_start.append([d[0], d[2], d[2]])
+          dbdata_end.append([d[0], d[3], d[3]])
+#          db.execute('DELETE FROM programs WHERE (channel_name=?) AND (start < ?) AND (end > ?)', (channel,d[2],d[2]))
+#          db.execute('DELETE FROM programs WHERE (channel_name=?) AND (start < ?) AND (end > ?)', (channel,d[3],d[3]))
+#          db.execute('INSERT INTO programs(channel_name, title, start, end, description) VALUES (?, ?, ?, ?, ?)', d)
+#          db.commit()
       
-    db.execute('DELETE FROM programs WHERE channel_name=?', (channel,))
+#    db.execute('DELETE FROM programs WHERE (channel_name=?) AND (start > ?)', (channel,now))
 #    s = "INSERT INTO programs(channel_name, title, start, end, description"
+    db.executemany('DELETE FROM programs WHERE (channel_name=?) AND (start < ?) AND (end > ?)', dbdata_start)
+    db.executemany('DELETE FROM programs WHERE (channel_name=?) AND (start < ?) AND (end > ?)', dbdata_end)
     db.executemany('INSERT INTO programs(channel_name, title, start, end, description) VALUES (?, ?, ?, ?, ?)', dbdata)
     db.commit()
     
